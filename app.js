6e8eea09939e5a0afa2fc373cde4e95be0ea882c
Abdullah690 <as250543@gmail.com>
2021-10-28 03:10:17 +0500

    javascript practise2

---
+++ app.js
@@ -0,0 +1,55 @@
+// alert("Error! Please enter valid password")
+
+
+
+//alert("Welcome to JS land... \n  Happy coading! " )
+
+
+
+// alert("Hello.. I can run JS through my web browser'console")
+
+
+
+// var b = "Jhone Doe";
+// alert(b);
+
+
+
+// var c = "15 year old";
+//  alert(c);
+
+
+
+// var p = "Certified Mobile Application Development";
+// alert(
+//     p
+// );
+
+
+
+// var x = "PIZZA \n PIZZ \n PIZ \n PI \n P" ;
+// alert (x);
+
+
+
+// var email = "My email address is example@example.com";
+// alert(email);
+
+
+
+// alert("I am trying to learn from the book A smarter \n way to learn Javascript");
+
+
+
+// var design = "--------------------ஜ۩۞۩ஜ--------------------";
+// alert(design);
+
+
+
+// var v =" Iam 17 year old";
+// alert(v);
+
+
+
+// var r ="you have visited this site 14 time";
+// alert(r);
